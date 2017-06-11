55e3f9023c8a74d85e20c47f5aafd17d587d226e
김서영 <kseoy4046@naver.com>
2017-06-11 16:40:52 +0900

    [반응형] 수정

--- ajong/common/top_login.php
+++ ajong/common/top_login.php
@@ -3,19 +3,19 @@
 if(!isset($_SESSION['user_session']))
 {
 	?>
- 	<a href="../mem/login.php">로그인</a> | <a href="../mem/register.php">회원가입</a>
+ 	<a id="top" href="../mem/login.php">로그인</a> | <a id="top" href="../mem/register.php">회원가입</a>
 	<?php
 }
 else
 {
 	if($_SESSION['user_session']=="admin"){
 	?>
-	<a href="../mem/logout.php?logout=true">로그아웃</a> | <a href="../mem/manage.php">회원관리</a>
+	<a id="top" href="../mem/logout.php?logout=true">로그아웃</a> | <a id="top" href="../mem/manage.php">회원관리</a>
 	<?php
 	}
 	else {
 	?>
-	<a href="../mem/logout.php?logout=true">로그아웃</a> | <a href="../mem/mypage.php">마이페이지</a>
+	<a id="top" href="../mem/logout.php?logout=true">로그아웃</a> | <a id="top" href="../mem/mypage.php">마이페이지</a>
 	<?php
 	}
 }
